e633e63a35d2eee3ee4b7e237d0b58ec62d5f67b
NitaAguirre <167264249+NitaAguirre@users.noreply.github.com>
2025-10-29 20:30:25 -0400

    Add HttpResponse import and update registro_equipo view

--- proyectoprueba/Participacion/views.py
+++ proyectoprueba/Participacion/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .forms import EquipoForm
 
-# Create your views here.
+def registro_equipo(request):
+    if request.method == 'POST':
+        form = EquipoForm(request.POST)
+        if form.is_valid():
+            nomEquipo = form.cleaned_data['nomEquipo']
+            nombreJefe = form.cleaned_data['nombreJefe']
+            membresia = form.cleaned_data['membresia']
+            cantidad = form.cleaned_data['cantidad']
+            total = membresia * cantidad
+
+            datos = {
+                'nomEquipo': nomEquipo,
+                'nombreJefe': nombreJefe,
+                'total': total
+            }
+            return render(request, 'Participacion/datosrecibidos.html', {'datos': datos})
+    else:
+        form = EquipoForm()
+
+    return render(request, 'Participacion/formulario.html', {'form': form})
