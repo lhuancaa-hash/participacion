fe9a96a8daf3f134ee49df9c0483ef064b9ad950
NitaAguirre <167264249+NitaAguirre@users.noreply.github.com>
2025-10-29 23:10:45 -0400

    Comente los anteriores URL'S, solo esta para la APP Participacion
    Comment out existing URL patterns for future reference.

--- proyectoprueba/proyectoprueba/urls.py
+++ proyectoprueba/proyectoprueba/urls.py
@@ -16,21 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pruebaapp import views
+#from pruebaapp import views
 from Participacion import views
 
 #A MANERA DE LIBRERIA
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('saludo/', views.saludo),
-    path('saludo_usuario/',views.info_usuario),
-    path('saludov2/<str:nombre>/<int:edad>/',views.saludo_mejorado),
-    path('tabla/<int:numero>',views.tabla_multiplicar),
-    path('saludo2/', views.saludo2, name='saludo2'),
-    path('informacion/', views.info, name='informacion'),
-    path('nuevo_saludo/<str:nombre>/<int:edad>/',views.nuevo_saludo, name='nuevo_saludo'),
-    path('tabla_multi/<int:num>/',views.tabla_producto,name='tabla_product'),
-    path('formulario/',views.formulario_cliente, name='formulario_cliente'),
-    path('nuevo/',views.crear_cliente,name='crear_cliente'),
+    #path('admin/', admin.site.urls),
+    #path('saludo/', views.saludo),
+    #path('saludo_usuario/',views.info_usuario),
+    #path('saludov2/<str:nombre>/<int:edad>/',views.saludo_mejorado),
+    #path('tabla/<int:numero>',views.tabla_multiplicar),
+    #path('saludo2/', views.saludo2, name='saludo2'),
+    #path('informacion/', views.info, name='informacion'),
+    #path('nuevo_saludo/<str:nombre>/<int:edad>/',views.nuevo_saludo, name='nuevo_saludo'),
+    #path('tabla_multi/<int:num>/',views.tabla_producto,name='tabla_product'),
+    #path('formulario/',views.formulario_cliente, name='formulario_cliente'),
+    #path('nuevo/',views.crear_cliente,name='crear_cliente'),
     path('formulario_equipo/', views.registro_equipo, name='registrar_equipo')
 ]
